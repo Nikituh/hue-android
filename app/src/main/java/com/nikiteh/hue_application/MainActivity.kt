d4602d02600ef2cf0b44b1c2e61b939c2c0abdbb
Aare Undo <aareundo@Aares-MacBook-Pro.local>
2018-07-13 19:07:06 +0300

    initial commit: blank project with dependencies

---
+++ app/src/main/java/com/nikiteh/hue_application/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nikiteh.hue_application
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
