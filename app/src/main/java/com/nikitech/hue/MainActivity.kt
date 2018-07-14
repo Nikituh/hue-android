bee10272fcdbed470d51312caa9823ef36308863
Aare Undo <aare@carto.com>
2018-07-14 11:18:05 +0300

    hue conversion

--- app/src/main/java/com/nikitech/hue/MainActivity.kt
+++ app/src/main/java/com/nikitech/hue/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.nikitech.hue
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.madrapps.pikolo.listeners.SimpleColorSelectionListener
-
+import com.nikitech.hue.networking.Networking
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,9 +26,17 @@ class MainActivity : AppCompatActivity() {
         contentView!!.picker.setColorSelectionListener(object : SimpleColorSelectionListener() {
             override fun onColorSelected(color: Int) {
 
-                val hexColor = String.format("#%06X", 0xFFFFFF and color)
-                println("Selected: $hexColor")
+//                val hexColor = String.format("#%06X", 0xFFFFFF and color)
+//                println("Selected: $hexColor")
+                val red = Color.red(color)
+                val green = Color.green(color)
+                val blue = Color.blue(color)
 
+                val array = floatArrayOf(0f, 0f, 0f)
+                val asdf = Color.colorToHSV(color, array)
+
+//                val hue = getHue(red, green, blue)
+                Networking.INSTANCE.update(array[0].toInt())
             }
         })
     }
@@ -35,4 +44,31 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
     }
+
+    fun getHue(red: Int, green: Int, blue: Int): Int {
+
+        val min = Math.min(Math.min(red, green), blue)
+        val max = Math.max(Math.max(red, green), blue)
+
+        if (min == max) {
+            return 0
+        }
+
+        var hue = 0
+        hue = if (max == red) {
+            (green - blue) / (max - min);
+
+        } else if (max == green) {
+            2 + (blue - red) / (max - min);
+
+        } else {
+            4 + (red - green) / (max - min);
+        }
+
+        hue *= 60
+        if (hue < 0) hue += 360
+
+        return hue
+    }
+
 }
--- app/src/main/java/com/nikitech/hue/networking/Networking.kt
+++ app/src/main/java/com/nikitech/hue/networking/Networking.kt
@@ -1,7 +1,15 @@
 package com.nikitech.hue.networking
 
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.android.extension.responseJson
+import com.github.kittinunf.fuel.core.Method
+import com.github.kittinunf.fuel.core.Request
 import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.fuel.httpPut
 import com.github.kittinunf.result.Result
+import com.google.gson.Gson
+import java.net.URL
+import java.util.*
 
 class Networking {
 
@@ -24,4 +32,45 @@ class Networking {
 
         return ""
     }
+
+    private val queue = PriorityQueue<Int>()
+
+    private val ip = "http://192.168.1.143/"
+    private val user = "2axlCppakzSFddi19q86ixCt0WiFODG6GaQ-ST1r"
+    private val groupUrl = "/groups/0/action"
+
+    fun update(color: Int) {
+
+        if (!queue.isEmpty()) {
+            queue.add(color)
+            return
+        }
+
+        sendData(color)
+    }
+
+    private fun sendData(color: Int) {
+        val request = Request()
+        request.httpMethod = Method.PUT
+        request.url = URL( ip + "api/" + user + groupUrl)
+
+        val body = "{\"hue\":$color, \"sat\": 254, \"bri\":150}"
+        request.body(body)
+
+        Fuel.request(request).responseJson {_, _, result ->
+            result.fold(success = { json ->
+                val string = json.array().toString()
+
+                if (!queue.isEmpty()) {
+                    sendData(queue.poll())
+                }
+
+                println("Success: " + string)
+            }, failure = { error ->
+                println("Error" + error.toString())
+            })
+        }
+    }
+
+
 }
